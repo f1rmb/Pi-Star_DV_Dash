21484c48b351641edcbce05478cb65212203bb93
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2020-08-18 13:56:57 +0200

    Add forgotten file.

---
+++ dstarrepeater/hw_info.php
@@ -0,0 +1,30 @@
+<?php
+// Get the CPU temp and colour the box accordingly...
+$cpuTempCRaw = exec('cat /sys/class/thermal/thermal_zone0/temp');
+if ($cpuTempCRaw > 1000) { $cpuTempC = round($cpuTempCRaw / 1000, 1); } else { $cpuTempC = round($cpuTempCRaw, 1); }
+$cpuTempF = round(+$cpuTempC * 9 / 5 + 32, 1);
+if ($cpuTempC < 50) { $cpuTempHTML = "<td style=\"background: #1d1\">".$cpuTempC."&deg;C/".$cpuTempF."&deg;F</td>\n"; }
+if ($cpuTempC >= 50) { $cpuTempHTML = "<td style=\"background: #fa0\">".$cpuTempC."&deg;C/".$cpuTempF."&deg;F</td>\n"; }
+if ($cpuTempC >= 69) { $cpuTempHTML = "<td style=\"background: #f00\">".$cpuTempC."&deg;C/".$cpuTempF."&deg;F</td>\n"; }
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/config/language.php');        // Translation Code
+
+$cpuLoad = sys_getloadavg();
+?>
+<h2><?php echo $lang['hardware_info'];?></h2>
+<table style="table-layout: fixed;">
+    <tr>
+	<th><a class="tooltip" href="#"><?php echo $lang['hostname'];?><br /><span><b>System IP Address:<br /><?php echo str_replace(',', ',<br />', exec('hostname -I'));?></b></span></a></th>
+	<th><a class="tooltip" href="#"><?php echo $lang['kernel'];?><span><b>Release</b>This is the version<br />number of the Linux Kernel running<br />on this Raspberry Pi.</span></a></th>
+	<th colspan="2"><a class="tooltip" href="#"><?php echo $lang['platform'];?><span><b>Uptime:<br /><?php echo str_replace(',', ',<br />', exec('uptime -p'));?></b></span></a></th>
+	<th colspan="2"><a class="tooltip" href="#"><?php echo $lang['cpu_load'];?><span><b>CPU Load</b></span></a></th>
+	<th><a class="tooltip" href="#"><?php echo $lang['cpu_temp'];?><span><b>CPU Temp</b></span></a></th>
+    </tr>
+    <tr>
+	<td><?php echo php_uname('n');?></td>
+	<td><?php echo php_uname('r');?></td>
+	<td colspan="2"><?php echo exec('platformDetect.sh');?></td>
+	<td colspan="2">1m:<?php echo $cpuLoad[0];?> / 5m:<?php echo $cpuLoad[1];?> / 15m:<?php echo $cpuLoad[2];?></td>
+	<?php echo $cpuTempHTML; ?>
+    </tr>
+</table>
