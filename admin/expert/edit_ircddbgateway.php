3ff6c9cc6fd4e3ea9bc1382f54375c25f0400975
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2020-08-19 06:38:23 +0200

    Don't add quote to passwords, ircDDDBGateway leave them in the value.

--- admin/expert/edit_ircddbgateway.php
+++ admin/expert/edit_ircddbgateway.php
@@ -69,7 +69,7 @@ if($_POST) {
 				if ($value == '') { 
 					$content .= $key."= \n";
 					}
-				else if ((strcmp($key, 'xlxHostsFileUrl') == 0) || strstr($key, 'Password')) {
+				else if (strcmp($key, 'xlxHostsFileUrl') == 0)
 					$content .= $key."=\"".$value."\"\n";
 					}
 				else {
