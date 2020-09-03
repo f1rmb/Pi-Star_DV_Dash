9baf2fac18481c81fb04e7fe3b7fcb609d87dcbb
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2020-09-03 06:01:04 +0200

    Add dmesg output. Gzip each single file on creation (save /tmp RAM disk space).

--- admin/download_all_logs.php
+++ admin/download_all_logs.php
@@ -17,10 +17,11 @@ if ($_SERVER["PHP_SELF"] == "/admin/download_all_logs.php") {
     
     exec("sudo rm -rf ".$backupZip." 2>&1");
     exec("sudo rm -rf ".$backupDir." 2>&1");
-    exec("sudo mkdir -p ".$backupDir."/pi-star ".$backupDir."/nginx 2>&1");
-    exec("sudo cp /var/log/pi-star/* ".$backupDir."/pi-star 2>&1");
-    exec("sudo cp /var/log/nginx/error.log ".$backupDir."/nginx 2>&1");
-    exec("sudo /bin/sh -c 'cd ".$backupDir." && zip -r9 ".$backupZip." ./' 2>&1");
+    exec("sudo mkdir -p ".$backupDir."/pi-star ".$backupDir."/nginx ".$backupDir."/dmesg 2>&1");
+    exec("sudo /bin/bash -c 'for f in /var/log/pi-star/*; do cat \$f | gzip > ".$backupDir."/pi-star/`basename \$f`.gz; done' 2>&1");
+    exec("sudo /bin/bash -c 'cat /var/log/nginx/error.log | gzip > ".$backupDir."/nginx/error.log.gz' 2>&1");
+    exec("sudo /bin/bash -c 'dmesg | gzip > ".$backupDir."/dmesg/dmesg.txt.gz' 2>&1");
+    exec("sudo /bin/bash -c 'cd ".$backupDir." && zip -r9 ".$backupZip." ./ && rm -rf ".$backupDir."' 2>&1");
 
     if (file_exists($backupZip)) {
 	$hostNameInfo = exec('cat /etc/hostname');
