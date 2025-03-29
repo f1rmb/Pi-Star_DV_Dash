f582e3e9fc050984f3b5c44880f758e6112cdf05
Daniel Caujolle-Bert <f1rmb.daniel@naboo-homelinux.org>
2025-03-29 12:49:14 +0100

    Fix PHP warning/error.

--- admin/expert/mmdvm_remote_exec.php
+++ admin/expert/mmdvm_remote_exec.php
@@ -5,17 +5,16 @@ if (isset($_COOKIE['PHPSESSID']))
 }
 if (session_status() != PHP_SESSION_ACTIVE) {
     session_start();
-}
-
-if (!isset($_SESSION) || !is_array($_SESSION) || (count($_SESSION, COUNT_RECURSIVE) < 10)) {
-    session_id('pistardashsess');
-    session_start();
 
     include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
     include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
     include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
     include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';        // Translation Code
-    checkSessionValidity();
+}
+
+if (!isset($_SESSION) || !is_array($_SESSION) || (count($_SESSION, COUNT_RECURSIVE) < 10)) {
+    session_id('pistardashsess');
+    session_start();
 }
 
 $command = isset($_GET['command']) ? $_GET['command'] : '';
